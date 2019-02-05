7d51eb7e9eafae4e481079508771f14c8d246ee7
Erick Henriquez <henriquez93550@yahoo.com>
2019-02-05 08:21:14 -0600

    added vid and header

--- client/src/components/Homeheader.jsx
+++ client/src/components/Homeheader.jsx
@@ -1,18 +1,21 @@
-import React, { Component } from 'react';
-import coding from '../images/coding.mp4';
+import React, { Component } from "react";
+import coding from "../images/coding.mp4";
+import "./Homeheader.css";
 
 class Homeheader extends Component {
-    render() {
-        return (
-            <video id="background-video" loop autoPlay>
-                    <source src={coding} type="video/mp4" />
-                    <source src={coding} type="video/ogg" />
-                    Your browser does not support the video tag.
-                </video>
-               
-         
-        );
-      }
+  render() {
+    return (
+      <div className="video-container">
+        <div className="video">
+          <video id="background-video" loop autoPlay>
+            <source src={coding} type="video/mp4" />
+            <source src={coding} type="video/ogg" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Homeheader;
